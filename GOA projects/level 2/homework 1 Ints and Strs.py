4650f0d96978a3f1d3c0ff9864306b3e69aac475
akaki9 <akakilobjanidze8@gmail.com>
2024-10-30 19:51:26 +0400

    second commit

---
+++ GOA projects/level 2/homework 1 Ints and Strs.py
@@ -0,0 +1,19 @@
+#num1 = "10"
+#num2 = "20"
+
+#10 + 20 = 30
+
+#print(num1 + " + " + num2 + " = " + str(int(num1) + int(num2)))
+
+# 10 + 20 = 30
+# 10 * 20 = 200
+# 20 - 10 = 10
+# 20 / 10 = 2
+
+num1 = 10
+num2 = 20
+
+print("10 + 20 = " + str(num1 + num2))
+print("10 * 20 = " + str(num1 * num2))
+print("20 - 10 = " + str(num2 - num1))
+print("20 / 10 = " + str(num2 / num1))
